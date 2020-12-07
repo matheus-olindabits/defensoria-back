536e1870efcfaf2c84c3e9e84be532ce21a65076
matheus-olindabits <matheuslealcm@gmail.com>
2020-12-07 15:28:46 -0300

    ajustes

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -6,11 +6,6 @@ const UsuarioController = require('../app/controllers/UsuarioController');
 
 const routes =  new Router();
 
-routes.get('/', async (req,res) => {
-    // variavel tem o nome da tabela que será criada
-    return res.send('Hello World');
-});
-
 routes.post('/cadastro', UsuarioController.cadastroUsuario);
 
 module.exports = routes;
